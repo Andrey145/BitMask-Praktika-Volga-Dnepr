85b176ce72e9a7cf5c81d9e998fa4fdbbfeefb30
Andrey145 <90236154+Andrey145@users.noreply.github.com>
2024-02-14 18:18:35 +0400

    Add files via upload

--- BitMask.vue
+++ BitMask.vue
@@ -2,61 +2,87 @@
 export default {
 	name: 'BitMask',
 	props: {
-		mask: String,
-		activeColor: String,
-		inactiveColor: String
+		numBytes: {
+			type: String,
+			default: '1'
+		},
+		mask: {
+			type: String,
+			default: '0'
+		},
+		activeColor: {
+			type: String,
+			default: '#00ff00'
+		},
+		inactiveColor: {
+			type: String,
+			default: '#999999'
+		}
+	},
+	computed: {
+		numBits: function() {
+			let number = parseInt(this.numBytes)
+			if (isNaN(number))
+				return 0
+			return 8*number
+		},
+		maskDecimal: function() {
+			let decimal = parseInt(this.mask)
+			if (this.mask.substring(0, 2) == '0b' || this.mask.substring(0, 2) == '0B')
+				decimal = parseInt(this.mask.substring(2, decimal.length), 2)
+			if (this.mask.substring(0, 2) == '0o' || this.mask.substring(0, 2) == '0O')
+				decimal = parseInt(this.mask.substring(2, decimal.length), 8)
+			if (this.mask.substring(0, 2) == '0x' || this.mask.substring(0, 2) == '0X')
+				decimal = parseInt(this.mask.substring(2, decimal.length), 16)
+			return decimal
+		}
 	},
 	methods: {
-		bitColor: function(index) {
-			let maskDecimal = parseInt(this.mask)
-			if (this.mask.substring(0, 2) == '0b' | this.mask.substring(0, 2) == '0B')
-				maskDecimal = parseInt(this.mask.substring(2, maskDecimal.length), 2)
-			if (this.mask.substring(0, 2) == '0o' | this.mask.substring(0, 2) == '0O')
-				maskDecimal = parseInt(this.mask.substring(2, maskDecimal.length), 8)
-			if (this.mask.substring(0, 2) == '0x' | this.mask.substring(0, 2) == '0X')
-				maskDecimal = parseInt(this.mask.substring(2, maskDecimal.length), 16)
+		isBitActive: function(index, maskDecimal) {
 			if (maskDecimal % (Math.pow(2, index)) >= (Math.pow(2, index)) / 2)
+				return true
+			return false
+		},
+		bitColor: function(index, maskDecimal) {
+			if (this.isBitActive(index, maskDecimal) == true)
 				return this.activeColor
 			return this.inactiveColor
+		},
+		valid: function(numBits, maskDecimal) {
+			if (numBits <= 0 || isNaN(maskDecimal))
+				return false
+			return true
 		}
 	}
 }
 </script>
 
 <template>
-<div className="square" v-bind:style="{'background-color': bitColor(16)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(15)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(14)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(13)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(12)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(11)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(10)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(9)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(8)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(7)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(6)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(5)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(4)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(3)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(2)}"></div>
-<div className="square" v-bind:style="{'background-color': bitColor(1)}"></div>
-<div className="squareEnd"></div>
+<div>
+	<div v-show="!valid(numBits, maskDecimal)" class="invalid">Invalid mask</div>
+	<div v-show="valid(numBits, maskDecimal)">
+		<div class="cell" v-for="item in numBits" :key="item" v-bind:style="{'background-color': bitColor(numBits+1-item, maskDecimal)}"></div>
+	</div>
+</div>
 </template>
 
 <style>
-.square {
+.cell {
 	display: inline-block;
 	float: left;
 	width: 7px;
-	height: 10px;
+	height: 12px;
 	border-top: 2px solid black;
 	border-bottom: 2px solid black;
+	border-right: 1px solid black;
+}
+.cell:first-child {
 	border-left: 2px solid black;
 }
-.squareEnd {
-	display: inline-block;
+.cell:nth-child(8n) {
+	border-right: 2px solid black;
+}
+.invalid {
 	float: left;
-	height: 12px;
-	border: 1px solid black;
 }
 </style>
